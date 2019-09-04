8d1a452dea1a8a28f6fbfe74550d8930230d977b
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-09-04 15:45:31 +0100

    seed users

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,3 +1,15 @@
 const { userData, languageData, repositoryData } = require("../data");
 
-exports.seed = function(connection) {};
+exports.seed = function(connection) {
+  return connection("users")
+    .insert(userData)
+    .returning("*")
+    .then(userRows => {
+      const langPromise = connection("languages")
+        .insert(languageData)
+        .returning("*");
+      //   const repoPromise = connection("repositories")
+      //     .insert(repositoryData)
+      //     .returning("*");
+    });
+};
