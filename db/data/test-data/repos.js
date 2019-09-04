54b4c80eb120412505a99a4d2a1b6d0fc3c63286
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-09-04 11:49:19 +0100

    add initial test data set

---
+++ db/data/test-data/repos.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    name: "hello-world",
+    owner: "jessjelly",
+    description: "my first repo",
+    languages: ["Node"]
+  },
+  {
+    name: "watson-image-analyser",
+    owner: "jessjelly",
+    description:
+      "express server that uses IBM's watson to tell you what's in an image",
+    languages: ["Node", "HTML", "CSS"]
+  },
+  {
+    name: "watson-clone",
+    owner: "weegembump",
+    description:
+      "clone of IBM's watson built using machine learning python packages and hosted using node",
+    languages: ["Node", "python"]
+  },
+  {
+    name: "my-custom-myspace-HTML-snippets",
+    owner: "weegembump",
+    description: "collection of cool html styling for my myspace page XD",
+    languages: ["HTML"]
+  }
+];
