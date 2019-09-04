44c11d05bbff8a330a8302ea83b0233987bfe450
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-09-04 16:34:26 +0100

    seeded all data but junction table

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,15 +1,23 @@
 const { userData, languageData, repositoryData } = require("../data");
-
+const { formatRepositoryData } = require("../utils/utils");
 exports.seed = function(connection) {
   return connection("users")
     .insert(userData)
     .returning("*")
     .then(userRows => {
+      console.log(userRows);
       const langPromise = connection("languages")
         .insert(languageData)
         .returning("*");
-      //   const repoPromise = connection("repositories")
-      //     .insert(repositoryData)
-      //     .returning("*");
+      const repoPromise = connection("repositories")
+        .insert(formatRepositoryData(repositoryData))
+        .returning("*");
+      return Promise.all([langPromise, repoPromise]);
+    })
+    .then(mystery => {
+      console.log(mystery);
     });
 };
+
+//HAVE: name, owner, description, languages
+//WANT: title, owner, description
