ba34944598c1c0ad8a6714ad35eb86a8f97e83ee
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-09-04 14:54:36 +0100

    add host key to connection object

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,13 @@ const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
   client: "pg",
-  connection: { database: "gitgram_test" },
-  seeds: { directory: "./db/seeds" }
+  seeds: { directory: "./db/seeds" },
+  migrations: { directory: "./db/migrations" }
 };
 
 const customConfig = {
-  test: { connection: { database: "gitgram_test" } },
-  development: { connection: { database: "gitgram" } }
+  test: { connection: { database: "gitgram_test", host: "localhost" } },
+  development: { connection: { database: "gitgram", host: "localhost" } }
 };
 
 module.exports = { ...baseConfig, ...customConfig[ENV] };
