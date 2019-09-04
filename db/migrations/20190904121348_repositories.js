28bd0513a7911fb940ab4b5c97b81d1347140f60
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-09-04 15:13:39 +0100

    rename id column

--- db/migrations/20190904121348_repositories.js
+++ db/migrations/20190904121348_repositories.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable("repositories", repository => {
-    repository.increments("repo_id");
+    repository.increments("repository_id");
     repository.string("title");
     repository.string("owner").references("users.username");
     repository.string("description");
