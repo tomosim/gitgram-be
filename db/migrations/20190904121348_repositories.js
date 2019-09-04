b4bd0bb09fbb1384ca2f9c6e3f1547ca994a03f7
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-09-04 15:07:09 +0100

    Add repo table and columns

---
+++ db/migrations/20190904121348_repositories.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("repositories", repository => {
+    repository.increments("repo_id");
+    repository.string("title");
+    repository.string("owner").references("users.username");
+    repository.string("description");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("repositories");
+};
